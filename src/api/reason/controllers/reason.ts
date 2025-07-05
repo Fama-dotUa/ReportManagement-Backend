58a8258e33c3674a853a9ac4f6d8c879dcbb8848
FAMA <andre.zloy2004@egmail.com>
2025-07-05 04:30:03 +0300

    Вынес причину в отдельную колекцию

---
+++ src/api/reason/controllers/reason.ts
@@ -0,0 +1,7 @@
+/**
+ * reason controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::reason.reason');
---
+++ src/api/reason/services/reason.ts
@@ -0,0 +1,7 @@
+/**
+ * reason service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::reason.reason');
